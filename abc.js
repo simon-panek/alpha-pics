57b345965dfcb99f16e405f6e7e57bdb30b87af4
Simon Panek <paneks19@gmail.com>
2021-04-15 10:12:30 -0700

    begin adding drop downs

--- abc.js
+++ abc.js
@@ -1,10 +1,12 @@
 //import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState, useRef } from 'react';
-import { StyleSheet, Text, View, Button, FlatList, Linking, TouchableOpacity, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, Linking, TouchableOpacity, TextInput, Image, } from 'react-native';
+import { Picker } from '@react-native-community/picker'
 import Header from './Header';
 
 export default function ABCGame(){
 
+  const [selectedValue, setSelectedValue] = useState('');
 
   return (
     <View>
@@ -15,9 +17,38 @@ export default function ABCGame(){
         </Text>
       </View>
       <View style={styles.mainButtonTwo}>
-        <Text style={styles.text}>
-          ABC Dropdown here
-        </Text>
+        <Picker
+          selectedValue={selectedValue}
+          style={{ height: 50, width: 150 }}
+          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+        >
+          <Picker.Item label='A' value='A' />
+          <Picker.Item label='B' value='B' />
+          <Picker.Item label='C' value='C' />
+          <Picker.Item label='D' value='D' />
+          <Picker.Item label='E' value='E' />
+          <Picker.Item label='F' value='F' />
+          <Picker.Item label='G' value='G' />
+          <Picker.Item label='H' value='H' />
+          <Picker.Item label='I' value='I' />
+          <Picker.Item label='J' value='J' />
+          <Picker.Item label='K' value='K' />
+          <Picker.Item label='L' value='L' />
+          <Picker.Item label='M' value='M' />
+          <Picker.Item label='N' value='N' />
+          <Picker.Item label='O' value='O' />
+          <Picker.Item label='P' value='P' />
+          <Picker.Item label='Q' value='Q' />
+          <Picker.Item label='R' value='R' />
+          <Picker.Item label='S' value='S' />
+          <Picker.Item label='T' value='T' />
+          <Picker.Item label='U' value='U' />
+          <Picker.Item label='V' value='V' />
+          <Picker.Item label='W' value='W' />
+          <Picker.Item label='X' value='X' />
+          <Picker.Item label='Y' value='Y' />
+          <Picker.Item label='Z' value='Z' />
+        </Picker>
       </View>
       <View style={styles.mainButtonThree}>
         <Text style={styles.text}>
